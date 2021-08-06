c116c752000123e12f1ba5f10b0e95cb4790b429
Julien <juliencaron@protonmail.com>
2021-08-06 21:26:49 +0200

    Add candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Election {
-    string candidate;
-
-    constructor() public {
-        candidate = "Candidate 1";
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
     }
 
-    function getCandidate() public view returns (string memory) {
-        return candidate;
+    // Read/Write candidates
+    mapping(uint256 => Candidate) public candidates;
+
+    // Used to access to the candidates mapping
+    uint256 public candidateCount;
+
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidateCount++;
+        candidates[candidateCount] = Candidate(candidateCount, _name, 0);
     }
 }
--- test/election.js
+++ test/election.js
@@ -1,10 +1,19 @@
 const Election = artifacts.require("Election.sol");
 
 contract('Election', accounts => {
-  it("Smoke test", async () => {
+  it("should initiate with right candidates", async () => {
     const instance = await Election.deployed()
-    const candidate = await instance.getCandidate()
 
-    assert.equal(candidate, "Candidate 1")
+    const candidate1 = await instance.candidates(1)
+
+    assert.equal(candidate1[0], 1, "Id is correct")
+    assert.equal(candidate1[1], "Candidate 1", "Name is correct")
+    assert.equal(candidate1[2], 0, "Vote count is correct")
+
+    const candidate2 = await instance.candidates(2)
+
+    assert.equal(candidate2[0], 2, "Id is correct")
+    assert.equal(candidate2[1], "Candidate 2", "Name is correct")
+    assert.equal(candidate2[2], 0, "Vote count is correct")
   });
 });
